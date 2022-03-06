13fc158beb90445fdc73ef7298b34780f40b4fea
Rivelino Riverea <chejoperez.com10@gmail.com>
2022-03-06 16:51:11 -0600

    Cambios

--- ServerNodeJs/globales.js
+++ ServerNodeJs/globales.js
@@ -1,8 +1,8 @@
 module.exports={
     global:{
-        USER_ACCESS_KEY_ID:'AKIAV4EMQWU6FRZ2M7LA',
-        SECRET_ACCESS_KEY:'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',
+        USER_ACCESS_KEY_ID:'',
+        SECRET_ACCESS_KEY:'',
         HOST:'localhost',
-        PASSWORD_DATABASE:'passWord_10'
+        PASSWORD_DATABASE:''
     }
 }
---
+++ ServerNodeJs/globales.js.bak
@@ -0,0 +1,8 @@
+module.exports={
+    global:{
+        USER_ACCESS_KEY_ID:'AKIAV4EMQWU6FRZ2M7LA',
+        SECRET_ACCESS_KEY:'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',
+        HOST:'localhost',
+        PASSWORD_DATABASE:'passWord_10'
+    }
+}
